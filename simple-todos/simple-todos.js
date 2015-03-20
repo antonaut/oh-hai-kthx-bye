d29014f10b490bd830c005d7bee308883a38a15d
Fredrik <fredde_wallen@hotmail.com>
2015-03-20 15:02:30 +0100

    Removed old code from the counter applicacion

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -74,19 +74,7 @@ if (Meteor.isClient) {
       return this.owner === Meteor.userId();
     }
   });
-
-  Template.hello.helpers({
-    counter: function() {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function() {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
