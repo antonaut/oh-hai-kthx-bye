24ef948e17f97e3684786eef41506eff0827343c
jojoman2 <fredde_wallen@hotmail.com>
2015-05-15 21:58:28 +0200

    Removed unnecessary constants

--- haiku/both/constants.js
+++ haiku/both/constants.js
@@ -1,11 +1,3 @@
-availableTextColors = [{name:"Black",code:"#000000"},
-					   {name:"Blue",code:"#0000FF"},
-					   {name:"Green",code:"#00FF00"},
-					   {name:"Red",code:"#FF0000"}];
-					   
-availableTextFonts = [{name:"Times New Roman",filePath:"fonts/TimesNewRoman.ttf"},
-					  {name:"Garamond",filePath:"fonts/Garamond.ttf"}];
-
 maxHaikusToShowOnEachPage = 100;
 
 facebookAppId = '100165010320303';
